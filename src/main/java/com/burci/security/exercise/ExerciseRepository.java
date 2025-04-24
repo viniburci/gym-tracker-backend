bb933453628505aef9a24b9582c0d5d02c6582d0
Vinicius Burci <vpburci@gmail.com>
2025-04-23 22:45:32 -0300

    remove Book folder

--- src/main/java/com/burci/security/exercise/ExerciseRepository.java
+++ src/main/java/com/burci/security/exercise/ExerciseRepository.java
@@ -3,5 +3,5 @@ package com.burci.security.exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long>{
-	
+
 }
